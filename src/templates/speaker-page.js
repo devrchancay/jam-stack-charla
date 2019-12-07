0482c3b77114b46479000e50d81803f59dc5b244
Ramon Chancay <ramon.chancay@gmail.com>
2019-12-07 18:43:52 -0500

    speaker page

--- src/templates/speaker-page.js
+++ src/templates/speaker-page.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
-const PageSpeaker = ({ data }) => <pre>{JSON.stringify(data, null, 4)}</pre>;
+import Layout from '../components/Layout';
+
+const PageSpeaker = ({ data }) => (
+  <Layout>
+    <img
+      src={data.wordpressWpSpeakers.avatar_urls.wordpress_96}
+      alt={data.wordpressWpSpeakers.title}
+    />
+    <h1>{data.wordpressWpSpeakers.title}</h1>
+    <p dangerouslySetInnerHTML={{ __html: data.wordpressWpSpeakers.content }} />
+  </Layout>
+);
 
 export const query = graphql`
   {
